60bdfab1e4e04a9caaacad326dfef1a7365df249
Lissy Langer <lissy.langer@campus.tu-berlin.de>
2020-05-31 13:52:20 +0200

    update nomenclature

--- single_building/run_SHEMS.jl
+++ single_building/run_SHEMS.jl
@@ -1,77 +1,83 @@
-include("main.jl")
+include("main.jl");
+include("single_building/SHEMS_optimizer.jl");
+include("single_building/SHEMS_optimizer_seco.jl");
+include("single_building/SHEMS_optimizer_sesu.jl");
 
-function yearly_SHEMS(h_start=1, h_end=8760, costfactor=1, objective=1, bc_violations=79, outputflag=true)
-    # Initialize technical setup________________________________________________
+function yearly_SHEMS(h_start=1, h_end=8760, objective=1, case=1, costfactor=1.0, outputflag=true, bc_violations=79)
+
+    # Initialize technical setup according to case______________________
     # set_SHEMS_parameters(h_start, h_end, h_predict, h_control, rolling_flag, costfactor)
-    sh, hp, fh, hw, b, m = set_SHEMS_parameters(h_start, h_end, (h_end-h_start)+1, (h_end-h_start)+1, false, costfactor, outputflag);
-    if objective==1
-        include("single_building/SHEMS_optimizer.jl")
-        # minimize costs
-        run= "Feedin+_battery+_mc";
+    sh, hp, fh, hw, b, m = set_SHEMS_parameters(h_start, h_end, (h_end-h_start)+1, (h_end-h_start)+1, false, case, costfactor, outputflag);
+
+    if objective==1   # minimize costs
         results  = SHEMS_optimizer(sh, hp, fh, hw, b, m);
-    elseif objective==2
-        include("single_building/SHEMS_optimizer_seco.jl")
-        # maximize self-consumption
-        run= "Feedin+_battery+_seco";
+    elseif objective==2  # maximize self-consumption
         results  = SHEMS_optimizer_seco(sh, hp, fh, hw, b, m, bc_violations);
-    elseif objective==3
-        include("single_building/SHEMS_optimizer_sesu.jl")
-        # maximize self-sufficiency
-        run= "Feedin+_battery+_sesu";
+    elseif objective==3  # maximize self-sufficiency
         results  = SHEMS_optimizer_sesu(sh, hp, fh, hw, b, m, bc_violations);
     end
+
     # write to results folder___________________________________________________
-    date=200530;
-    write_to_results_file(hcat(results, ones(size(results,1))*m.h_predict), m, date, run, costfactor)
+    write_to_results_file(hcat(results, ones(size(results,1))*m.h_predict), m, objective, case, costfactor)
     return nothing
 end
 
-function roll_SHEMS(h_start, h_end, h_predict, h_control, costfactor, outputflag=false)
-    include("SHEMS_optimizer.jl")
+function roll_SHEMS(h_start, h_end, h_predict, h_control, costfactor=1.0, outputflag=false, case=1)
+
     # Initialize technical setup__________________________________________________
     # set_SHEMS_parameters(h_start, h_end, h_predict, h_control, rolling_flag, costfactor, outputflag)
-    sh, hp, fh, hw, b, m = set_SHEMS_parameters(h_start, h_end, h_predict, h_control, true, costfactor, outputflag);
+    sh, hp, fh, hw, b, m = set_SHEMS_parameters(h_start, h_end, h_predict, h_control, true, costfactor, outputflag, case);
+
     # intitial run_______________________________________________________________
     sh.soc_b, sh.soc_fh, sh.soc_hw, results_new  = SHEMS_optimizer(sh, hp, fh, hw, b, m);
     results = results_new;
     sh.h_start = m.h_start + m.h_control;
+
     # loop runs for rest of horizon______________________________________________
     for h = sh.h_start:m.h_control:(m.h_end-m.h_predict)
         sh.h_start = h;
         sh.soc_b, sh.soc_fh, sh.soc_hw, results_new  = SHEMS_optimizer(sh, hp, fh, hw, b, m);
         results = vcat(results, results_new);
     end
+
     # write to results folder____________________________________________________
-    date=200530;
-    run= "Feedin+_battery+_mc";
-    write_to_results_file(hcat(results, ones(size(results,1))*m.h_predict), m, date, run, costfactor)
+    write_to_results_file(hcat(results, ones(size(results,1))*m.h_predict), m, objective=1, case, costfactor)
     return nothing
 end
 
-function set_SHEMS_parameters(h_start, h_end, h_predict, h_control, rolling_flag, costfactor, outputflag)
+function set_SHEMS_parameters(h_start, h_end, h_predict, h_control, rolling_flag, case=1, costfactor=1.0, outputflag=false)
+
     # Initialize technical setup________________________________________________
-    # SHEMS(costfactor, p_buy, p_sell, soc_b, soc_fh, soc_hw, h_start)
-    # set p_sell to zero for no feedin tariff, soc_b zero for no battery
-    sh = SHEMS(costfactor, 0.3f0, 0.1f0, 13.5f0, 22.0f0, 180.0f0, h_start);
-    # HeatPump(eta, rate_max)
-    hp = HeatPump(1.0f0, 3.0f0);
-    # ThermalStorage(eta, volume, loss, t_supply, soc_min, soc_max)
-    fh = ThermalStorage(1.0f0, 10.0f0, 0.045f0, 30.0f0, 20.0f0, 22.0f0);
-    # ThermalStorage(eta, volume, loss, t_supply, soc_min, soc_max)
-    hw = ThermalStorage(1.0f0, 180.0f0, 0.035f0, 45.0f0, 20.0f0, 180.0f0);
-    # Battery(eta, soc_min, soc_max, rate_max, loss)
-    # set soc_max, rate_max to zero for no battery
-    b = Battery(0.95f0, 0.0f0, 13.5f0, 3.3f0, 0.00003f0);
     # Model_SHEMS(h_start, h_end, h_predict, h_control, big, rolling_flag, mip_gap, output_flag, presolve_flag)
     m = Model_SHEMS(h_start, h_end,  h_predict, h_control, 60, rolling_flag, 0.005f0, outputflag, -1);
+    hp = HeatPump(1.0f0, 3.0f0);    # HeatPump(eta, rate_max)
+    fh = ThermalStorage(1.0f0, 10.0f0, 0.045f0, 30.0f0, 20.0f0, 22.0f0);    # ThermalStorage(eta, volume, loss, t_supply, soc_min, soc_max)
+    hw = ThermalStorage(1.0f0, 180.0f0, 0.035f0, 45.0f0, 20.0f0, 180.0f0);  # ThermalStorage(eta, volume, loss, t_supply, soc_min, soc_max)
+
+    if case==1 # base case
+        b = Battery(0.95f0, 0.0f0, 13.5f0, 3.3f0, 0.00003f0);  # Battery(eta, soc_min, soc_max, rate_max, loss)
+        sh = SHEMS(costfactor, 0.3f0, 0.1f0, 13.5f0, 22.0f0, 180.0f0, h_start); # SHEMS(costfactor, p_buy, p_sell, soc_b, soc_fh, soc_hw, h_start)
+    elseif case==2 # no battery
+        b = Battery(0.95f0, 0.0f0, 0.0f0, 0.0f0, 0.00003f0);    # set soc_max, rate_max to zero for no battery
+        sh = SHEMS(costfactor, 0.3f0, 0.1f0, 0.0f0, 22.0f0, 180.0f0, h_start);  # soc_b zero for no battery
+    elseif case==3 # no grid feed-in compensation
+        b = Battery(0.95f0, 0.0f0, 13.5f0, 3.3f0, 0.00003f0);   # Battery(eta, soc_min, soc_max, rate_max, loss)
+        sh = SHEMS(costfactor, 0.3f0, 0.0f0, 13.5f0, 22.0f0, 180.0f0, h_start); # set p_sell to zero for no feedin tariff
+    elseif case==3 # no battery and no grid feed-in compensation
+        b = Battery(0.95f0, 0.0f0, 0.0f0, 0.0f0, 0.00003f0);    # Battery(eta, soc_min, soc_max, rate_max, loss)
+        sh = SHEMS(costfactor, 0.3f0, 0.0f0, 0.0f0, 22.0f0, 180.0f0, h_start);  # set p_sell to zero for no feedin tariff
+    end
+
     return sh, hp, fh, hw, b, m
 end
 
-function write_to_results_file(all, m, date, run, costfactor=1)
-    CSV.write("single_building/results/$(date)_results_$(m.h_predict)_$(m.h_control)_$(m.h_start)-$(m.h_end)_$(run)_$(costfactor).csv", DataFrame(all),
+function write_to_results_file(results, m, objective=1, case=1)
+    date=200531;
+    CSV.write("single_building/results/$(date)_results_$(m.h_predict)_$(m.h_control)_$(m.h_start)-$(m.h_end)_$(objective)_$(case)_$(sh.costfactor).csv", DataFrame(all),
         header=["Temp_FH", "Vol_HW", "Soc_B", "V_HW_plus", "V_HW_minus", "T_FH_plus", "T_FH_minus", "profits", "COP_FH", "COP_HW",
         "PV_DE", "B_DE", "GR_DE", "PV_B", "PV_GR", "PV_HP","GR_HP", "B_HP", "HP_FH", "HP_HW",
         "month", "day", "hour", "horizon"]);
+
     return nothing
 end
 
